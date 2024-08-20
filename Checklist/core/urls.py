67dc91d4c4511173317ce87fc0749ff206f13698
Shreya Panchal <shreya.panchal@fxis.ai>
2024-08-20 14:52:03 +0530

    intial commit

--- Checklist/core/urls.py
+++ Checklist/core/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('',TestApiViews.as_view()),
     path('api/checklists',ChecklistsApiViews.as_view()),
     path('api/checklist/<int:pk>',ChecklistApiViews.as_view()),
-    path('api/checklistitems/create',ChecklistItemsCreateApiViews.as_view()),
-    path('api/checklistitem/<int:pk>',ChecklistItemsApiViews.as_view()),
+    path('api/checklist-items/create',ChecklistItemsCreateApiViews.as_view()),
+    path('api/checklist-item/<int:pk>',ChecklistItemsApiViews.as_view()),
 
 ]
