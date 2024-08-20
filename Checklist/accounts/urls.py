aeef6fd6d5ca210c2a72095d2615d9037691834e
Shreya Panchal <shreya.panchal@fxis.ai>
2024-08-20 16:53:10 +0530

    intial commit

--- Checklist/accounts/urls.py
+++ Checklist/accounts/urls.py
@@ -5,7 +5,10 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
+from accounts.views import RegisterAPIViews
 urlpatterns = [
     path('api/login/', TokenObtainPairView.as_view(), name='login_view'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh_view'),
+
+    path('api/register',RegisterAPIViews.as_view())
 ]
--- Checklist/accounts/views.py
+++ Checklist/accounts/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
 
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from accounts.serializers import UserRegisterSerializer
+
+from rest_framework_simplejwt.tokens import RefreshToken
 # Create your views here.
+
+class RegisterAPIViews(APIView):
+    serializer_class = UserRegisterSerializer
+
+    def post(self, request, format=None):
+        # creation code
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+
+            refresh = RefreshToken.for_user(user)
+
+            response_data = {
+                'refresh': str(refresh),
+                'access': str(refresh.access_token),
+                'user': serializer.data,
+            }
+
+
+            return Response(response_data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
