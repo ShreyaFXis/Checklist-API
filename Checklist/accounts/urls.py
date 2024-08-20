69a904416127c5aadc0adda89c35695ab12d5388
Shreya Panchal <shreya.panchal@fxis.ai>
2024-08-20 14:53:01 +0530

    intial commit

---
+++ Checklist/accounts/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+urlpatterns = [
+    path('api/login/', TokenObtainPairView.as_view(), name='login_view'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh_view'),
+]
